1596b8c9a96203ff0ca02ea202d2fab958c3a4b3
Audrey Rannou <audrey.rannou@gmail.com>
2022-06-20 17:54:38 +0200

    mise en place pâckage cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config("../.env");
 
 const express = require("express");
+const cors = require ("cors");
 const mongoose = require("mongoose");
 const helmet = require ("helmet");
 
@@ -24,19 +25,9 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-// Définition des headers CORS
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+
+  app.use(cors ());
+
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/sauces", sauceRoutes);
